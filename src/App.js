8128f8fc863756d16c24cc8da1dbf2904a1db9e7
Aizen <iansky3311@gmail.com>
2023-04-17 20:32:17 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import React, { useState } from "react";
 
 function App() {
+
   const [Counter, setCounter] = useState(1)
   const Onclick = () => {
     setCounter(prevCounter => prevCounter + 1)
     alert(`You clicked this button ${Counter} times`);
-    }
+  }
+
   return (
       <div>
         <header>
-          <img src="https://gazettereview.com/wp-content/uploads/2016/07/currency.jpg" alt="Currencies"></img>
+        <img src="https://gazettereview.com/wp-content/uploads/2016/07/currency.jpg" alt="Currencies"/>
           <button type="button" onClick={Onclick}>&#128512;</button>
         </header>
       </div>
